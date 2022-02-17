45554e568e98ec2c2651ca959765d97e517cbe47
Matthew Bryant <matt23488@gmail.com>
2022-02-17 15:50:11 -0500

    getting 3d to work. For some reason point lights don't seem to work with custom vertices, not sure why. Probably normals I'd guess.

--- src/components/Viewport.tsx
+++ src/components/Viewport.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { ReactNode, useEffect, useMemo, useState } from 'react';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { Canvas, useLoader } from '@react-three/fiber';
 
 import './css/Viewport.css';
 import ImageCanvasDisplay from './ImageCanvasDisplay';
 
 const Viewport: React.FC<ViewportProperties> = props => {
+    const [mode, setMode] = useState(ViewportMode.mesh);
+
+    const vertices = new Float32Array([
+        -1, -1, 1,
+        1, -1, 1,
+        1, 1, -1,
+        -1, -1, 1,
+        1, 1, -1,
+        -1, 1, -1,
+    ]);
+
+    let displayedComponent: ReactNode;
+    if (mode === ViewportMode.image)
+        displayedComponent = (
+            <ImageCanvasDisplay imageDataUrl={props.imageDataUrl} brightnessModifier={props.imageBrightnessModifier} sampleCount={props.imageSampleCount} />
+        );
+    else
+        displayedComponent = (
+            <Canvas>
+                <pointLight position={[0, 10, 0]} />
+                {/* <ambientLight /> */}
+                <mesh>
+                    <sphereBufferGeometry />
+                    <bufferGeometry attach="geometry">
+                        <bufferAttribute attachObject={['attributes', 'position']} count={vertices.length / 3} array={vertices} itemSize={3} />
+                    </bufferGeometry>
+                    <meshStandardMaterial attach="material" color="hotpink" />
+                </mesh>
+            </Canvas>
+        );
+
     return (
         <div className="Viewport">
-            <ImageCanvasDisplay imageDataUrl={props.imageDataUrl} brightnessModifier={props.imageBrightnessModifier} sampleCount={props.imageSampleCount} />
+            <div className="mode-buttons">
+                <button className="button mode-button" onClick={() => setMode(ViewportMode.image)}>Image</button>
+                <button className="button mode-button" onClick={() => setMode(ViewportMode.mesh)}>Mesh</button>
+            </div>
+            
+            {displayedComponent}
         </div>
     );
 };
+
+enum ViewportMode {
+    image,
+    mesh
+}
 
 export interface ViewportProperties {
     imageDataUrl: string;
